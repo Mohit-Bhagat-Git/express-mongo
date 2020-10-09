e9df51f44603ebf38d345de33e789b26bfa11d30
Mohit221989 <Inmarsat@123$>
2020-10-09 12:14:34 +0530

    Basic Router setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const app = express();
+const router = require('./router-config')
 app.listen(2020, ()=>{console.log("Server started on 2020")})
+app.use('/', router)
