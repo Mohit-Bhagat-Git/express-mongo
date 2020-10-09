9592a917aae6c954ed91608063f0ae6b6d377040
Mohit221989 <Inmarsat@123$>
2020-10-09 12:53:07 +0530

    Error handling basic way

--- es6/index.js
+++ es6/index.js
@@ -4,4 +4,10 @@ import Router from './router-config.js'
 const app = express();
 app.listen(1111, ()=>{console.log("ES6 Server started on port:1111")})
 app.use('/es6', Router)
+app.use((req, res, next)=>{
+    res.statusCode= 404
+    res.send({
+        error: "API Not found"
+    })
+})
 
